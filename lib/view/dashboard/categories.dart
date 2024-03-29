4731419c3126e5f0d968e9c98f4bee92ce3fe4f8
Shayan Saeed <115692639+shayansaeed123@users.noreply.github.com>
2024-03-29 12:20:04 +0500

    category
    category

--- lib/view/dashboard/categories.dart
+++ lib/view/dashboard/categories.dart
@@ -18,10 +18,10 @@ class _CategoriesPageState extends State<CategoriesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      backgroundColor: Color(0xFFffb64d),
+      // backgroundColor: Color(0xFFffb64d),
       appBar: AppBar(
         backgroundColor: Color(0xFFffb64d),
-        title: Text('Home'),
+        title: Text('Categories'),
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {
@@ -37,73 +37,89 @@ class _CategoriesPageState extends State<CategoriesPage> {
         ],
         ),
         drawer: SideBar(),
-      body: Row(
-        children: [
-          Padding(
-            padding:  EdgeInsets.all(MediaQuery.of(context).size.height * .01),
-            child: 
-            Container(
-              width: MediaQuery.of(context).size.width * .44,
-              height: MediaQuery.of(context).size.height * .261,
-              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Card(
-                    margin: EdgeInsets.all(5),
-                    child: Image.asset('assets/log.png',height: MediaQuery.of(context).size.height * .18,width: double.infinity,),
-                    elevation: 7,
-                  ),
-                  Expanded(
-                    child: InkWell(
-                      onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ItemsPage(),));
-              },
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                // eb8815 f5d12d
+                Color(0xFFe5af55),
+                Color(0xFFe3e18e),
+              ],
+            ),
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:  EdgeInsets.all(MediaQuery.of(context).size.height * .01),
+              child: 
+              Container(
+                width: MediaQuery.of(context).size.width * .44,
+                height: MediaQuery.of(context).size.height * .261,
+                decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Card(
+                      margin: EdgeInsets.all(5),
+                      child: Image.asset('assets/log.png',height: MediaQuery.of(context).size.height * .18,width: double.infinity,),
+                      elevation: 7,
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ItemsPage(),));
+                },
+                        child: Container(
+                          width: double.infinity,
+                          // height: MediaQuery.of(context).size.height*0.05,
+                          margin: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
+                          // padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.purple.shade800,borderRadius: BorderRadius.circular(8.0)),
+                          child: Center(child: Text('Fruits',style: TextStyle(color: Colors.white),)),
+                        
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
+              child: Container(
+                width: MediaQuery.of(context).size.width * .44,
+                height: MediaQuery.of(context).size.height * .261,
+                decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Card(
+                      margin: EdgeInsets.all(5),
+                      child: Image.asset('assets/log.png',height: MediaQuery.of(context).size.height * .18,width: double.infinity,),
+                      elevation: 7,
+                    ),
+                    Expanded(
                       child: Container(
                         width: double.infinity,
                         // height: MediaQuery.of(context).size.height*0.05,
                         margin: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
-                        // padding: EdgeInsets.all(10),
+                        // padding: EdgeInsets.all(MediaQuery.of(context).size.height * .02),
                         decoration: BoxDecoration(color: Colors.purple.shade800,borderRadius: BorderRadius.circular(8.0)),
                         child: Center(child: Text('Fruits',style: TextStyle(color: Colors.white),)),
                       
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
-            child: Container(
-              width: MediaQuery.of(context).size.width * .44,
-              height: MediaQuery.of(context).size.height * .261,
-              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Card(
-                    margin: EdgeInsets.all(5),
-                    child: Image.asset('assets/log.png',height: MediaQuery.of(context).size.height * .18,width: double.infinity,),
-                    elevation: 7,
-                  ),
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      // height: MediaQuery.of(context).size.height*0.05,
-                      margin: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
-                      // padding: EdgeInsets.all(MediaQuery.of(context).size.height * .02),
-                      decoration: BoxDecoration(color: Colors.purple.shade800,borderRadius: BorderRadius.circular(8.0)),
-                      child: Center(child: Text('Fruits',style: TextStyle(color: Colors.white),)),
-                    
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ), 
-        ],
+            ), 
+          ],
+        ),
       )
       
     );
--- lib/view/dashboard/items.dart
+++ lib/view/dashboard/items.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:picture_dictionary/res/reusablehomebtn.dart';
@@ -17,170 +16,211 @@ class _ItemsPageState extends State<ItemsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: Color(0xFFffb64d),
-      appBar: AppBar(
-        backgroundColor: Color(0xFFffb64d),
-        title: Text('Home'),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            _scaffoldKey.currentState!.openDrawer();
-          },
+        key: _scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Color(0xFFffb64d),
+          title: Text('Categories Name'),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              _scaffoldKey.currentState!.openDrawer();
+            },
+          ),
+          automaticallyImplyLeading: false,
+          actions: [
+            reusablehomeBtn('ENG', () {}),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .01,
+            ),
+            reusablehomeBtn('SEARCH', () {}),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .01,
+            ),
+          ],
         ),
-        automaticallyImplyLeading: false,
-        actions: [
-          reusablehomeBtn('ENG', (){}),
-          SizedBox(width: MediaQuery.of(context).size.width * .01,),
-          reusablehomeBtn('SEARCH', (){}),
-          SizedBox(width: MediaQuery.of(context).size.width * .01,),
-        ],
-        ),
-      drawer: SideBar(),
-      body: Container(
-  decoration: BoxDecoration(
-    gradient: LinearGradient(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      colors: [
-        Color(0xFFe5af55),
-        Color(0xFFe3e18e),
-      ],
-    ),
-  ),
-  child: GridView.builder(
-    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-    ),
-    itemBuilder: (context, index) {
-      return Stack(
-        children: [
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.03,
-            left: MediaQuery.of(context).size.width * 0.03,
-            right: MediaQuery.of(context).size.width * 0.03,
-            child: Transform.rotate(
-              angle: 45 * 3.1415927 / 180,
-              child: Container(
-                width: MediaQuery.of(context).size.width * .40,
-                height: MediaQuery.of(context).size.height * .261,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(11),
-                ),
-              ),
+        drawer: SideBar(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFFe5af55),
+                Color(0xFFe3e18e),
+              ],
             ),
           ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.03,
-            left: MediaQuery.of(context).size.width * 0.03,
-            right: MediaQuery.of(context).size.width * 0.03,
-            child: Container(
-              width: MediaQuery.of(context).size.width * .44,
-              height: MediaQuery.of(context).size.height * .261,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(11),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 10),
+                itemCount: 10,
+            itemBuilder: (context, index) {
+              return Stack(
                 children: [
-                  Card(
-                    margin: EdgeInsets.all(5),
-                    child: Image.asset(
-                      'assets/log.png',
-                      height: MediaQuery.of(context).size.height * .18,
-                      width: double.infinity,
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.03,
+                    left: MediaQuery.of(context).size.width * 0.03,
+                    right: MediaQuery.of(context).size.width * 0.03,
+                    child: 
+                    Transform.rotate(
+                      angle: 45  / 180,
+                      // 45 * 3.1415927 / 180,
+                      child: 
+                      Container(
+                        width: MediaQuery.of(context).size.width * .44,
+                        height: MediaQuery.of(context).size.height * .20,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFffb64d),
+                          borderRadius: BorderRadius.circular(11),
+                        ),
+                      ),
                     ),
-                    elevation: 7,
                   ),
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.purple.shade800,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Fruits',
-                        style: TextStyle(color: Colors.white),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.03,
+                    left: MediaQuery.of(context).size.width * 0.03,
+                    right: MediaQuery.of(context).size.width * 0.03,
+                    child: 
+      //               Card(
+      //   elevation: 2.0,
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.stretch,
+      //     children: <Widget>[
+      //       Container(
+      //           width: MediaQuery.of(context).size.width * .44,
+      //                 height: MediaQuery.of(context).size.height * .261,
+      //         color: Colors.blue,
+      //         child: Center(
+      //           child: Text(
+      //             'Item $index',
+      //             style: TextStyle(color: Colors.white),
+      //           ),
+      //         ),
+      //       ),
+      //       // Padding(
+      //       //   padding: EdgeInsets.all(8.0),
+      //         // child: 
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             // Button action
+      //           },
+      //           child: Text('Button $index'),
+      //         ),
+      //       // ),
+      //     ],
+      //   ),
+      // ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * .44,
+                      height: MediaQuery.of(context).size.height * .20,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Card(
+                            margin: EdgeInsets.all(5),
+                            child: Image.asset(
+                              'assets/log.png',
+                              height: MediaQuery.of(context).size.height * .15,
+                              width: double.infinity,
+                            ),
+                            elevation: 7,
+                            color: Colors.white,
+                          ),
+                          Expanded(
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.purple.shade800,
+                                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0), bottomRight: Radius.circular(20.0)),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Fruits',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ],
-              ),
-            ),
+              );
+            },
           ),
-        ],
-      );
-    },
-  ),
-)
+        )
 
-      // Container(
-      //   decoration: BoxDecoration(
-      //       gradient: LinearGradient(
-      //         begin: Alignment.topCenter,
-      //         end: Alignment.bottomCenter,
-      //         colors: [
-      //           // eb8815 f5d12d
-      //           Color(0xFFe5af55),
-      //           Color(0xFFe3e18e),
-      //         ],
-      //       ),
-      //     ),
-      //   child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount
-      //   (crossAxisCount: 2,), 
-      //   itemBuilder: (context, index) {
-      //     return Stack(
-      //     children: [
-      //       Positioned(
-      //         top: MediaQuery.of(context).size.height * 0.03,
-      //       left: MediaQuery.of(context).size.width * 0.03,
-      //       right: MediaQuery.of(context).size.width * 0.03,
-      //         child: Transform.rotate(
-      //           angle: 45 * 3.1415927 / 180,
-      //           child: Container(
-      //             width: MediaQuery.of(context).size.width * .40,
-      //             height: MediaQuery.of(context).size.height * .261,
-      //             decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
-      //           ),
-      //         ),
-      //       ),
-      //       Positioned(
-      //         top: MediaQuery.of(context).size.height * 0.03,
-      //       left: MediaQuery.of(context).size.width * 0.03,
-      //       right: MediaQuery.of(context).size.width * 0.03,
-      //         child: Container(
-      //           width: MediaQuery.of(context).size.width * .44,
-      //           height: MediaQuery.of(context).size.height * .261,
-      //           decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
-      //           child: Column(
-      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //             children: [
-      //               Card(
-      //                 margin: EdgeInsets.all(5),
-      //                 child: Image.asset('assets/log.png',height: MediaQuery.of(context).size.height * .18,width: double.infinity,),
-      //                 elevation: 7,
-      //               ),
-      //               Expanded(
-      //                 child: Container(
-      //                   width: double.infinity,
-      //                   margin: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
-      //                   // padding: EdgeInsets.all(10),
-      //                   decoration: BoxDecoration(color: Colors.purple.shade800,borderRadius: BorderRadius.circular(8.0)),
-      //                   child: Center(child: Text('Fruits',style: TextStyle(color: Colors.white),)),
-      //                 ),
-      //               )
-      //             ],
-      //           ),
-      //         ),
-      //       ),
-      //     ],
-      //   );
-      //   },)
-      // )
-      
-    );
+        // Container(
+        //   decoration: BoxDecoration(
+        //       gradient: LinearGradient(
+        //         begin: Alignment.topCenter,
+        //         end: Alignment.bottomCenter,
+        //         colors: [
+        //           // eb8815 f5d12d
+        //           Color(0xFFe5af55),
+        //           Color(0xFFe3e18e),
+        //         ],
+        //       ),
+        //     ),
+        //   child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount
+        //   (crossAxisCount: 2,),
+        //   itemBuilder: (context, index) {
+        //     return Stack(
+        //     children: [
+        //       Positioned(
+        //         top: MediaQuery.of(context).size.height * 0.03,
+        //       left: MediaQuery.of(context).size.width * 0.03,
+        //       right: MediaQuery.of(context).size.width * 0.03,
+        //         child: Transform.rotate(
+        //           angle: 45 * 3.1415927 / 180,
+        //           child: Container(
+        //             width: MediaQuery.of(context).size.width * .40,
+        //             height: MediaQuery.of(context).size.height * .261,
+        //             decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
+        //           ),
+        //         ),
+        //       ),
+        //       Positioned(
+        //         top: MediaQuery.of(context).size.height * 0.03,
+        //       left: MediaQuery.of(context).size.width * 0.03,
+        //       right: MediaQuery.of(context).size.width * 0.03,
+        //         child: Container(
+        //           width: MediaQuery.of(context).size.width * .44,
+        //           height: MediaQuery.of(context).size.height * .261,
+        //           decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(11),),
+        //           child: Column(
+        //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //             children: [
+        //               Card(
+        //                 margin: EdgeInsets.all(5),
+        //                 child: Image.asset('assets/log.png',height: MediaQuery.of(context).size.height * .18,width: double.infinity,),
+        //                 elevation: 7,
+        //               ),
+        //               Expanded(
+        //                 child: Container(
+        //                   width: double.infinity,
+        //                   margin: EdgeInsets.all(MediaQuery.of(context).size.height * .01),
+        //                   // padding: EdgeInsets.all(10),
+        //                   decoration: BoxDecoration(color: Colors.purple.shade800,borderRadius: BorderRadius.circular(8.0)),
+        //                   child: Center(child: Text('Fruits',style: TextStyle(color: Colors.white),)),
+        //                 ),
+        //               )
+        //             ],
+        //           ),
+        //         ),
+        //       ),
+        //     ],
+        //   );
+        //   },)
+        // )
+
+        );
   }
 }
