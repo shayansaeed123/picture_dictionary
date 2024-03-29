25458559cd3cd2b4566bfec3823838ff8592ce88
Shayan Saeed <115692639+shayansaeed123@users.noreply.github.com>
2024-03-29 12:22:42 +0500

    update
    update

--- lib/view/dashboard/itemdetails.dart
+++ lib/view/dashboard/itemdetails.dart
@@ -1,2 +1,17 @@
 
 
+import 'package:flutter/cupertino.dart';
+
+class ItemDetails extends StatefulWidget {
+  const ItemDetails({super.key});
+
+  @override
+  State<ItemDetails> createState() => _ItemDetailsState();
+}
+
+class _ItemDetailsState extends State<ItemDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
