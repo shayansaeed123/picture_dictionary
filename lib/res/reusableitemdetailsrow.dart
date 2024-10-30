440c8ec758336c285b12bb01ec8436aaf119437d
Shayan Saeed <bhattishayan274@gmail.com>
2024-10-30 17:33:44 +0500

    updated
    updated

--- lib/res/reusableitemdetailsrow.dart
+++ lib/res/reusableitemdetailsrow.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:picture_dictionary/controller/color_controller.dart';
 
-Widget reusableitemdetailsrow(String name, Color  color, bordercolor, BuildContext context, Function onTap) {
+Widget reusableitemdetailsrow(String name, Color color, bordercolor,
+    BuildContext context, Function onTap) {
   return Row(
     children: [
       Expanded(
@@ -13,9 +13,13 @@ Widget reusableitemdetailsrow(String name, Color  color, bordercolor, BuildConte
           height: MediaQuery.of(context).size.height * .05,
           decoration: BoxDecoration(
             color: color,
-             borderRadius: BorderRadius.circular(11),
+            borderRadius: BorderRadius.circular(11),
           ),
-          child: Center(child: Text(name,style: TextStyle(color: ColorController().whiteColor,fontSize: 18),)),
+          child: Center(
+              child: Text(
+            name,
+            style: TextStyle(color: ColorController().whiteColor, fontSize: 18),
+          )),
         ),
       ),
       InkWell(
@@ -23,15 +27,17 @@ Widget reusableitemdetailsrow(String name, Color  color, bordercolor, BuildConte
           onTap();
         },
         child: Container(
-                        margin: EdgeInsets.only(right: MediaQuery.of(context).size.height * .01,),
-                        padding: EdgeInsets.all(MediaQuery.of(context).size.height * .006),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: bordercolor,width: 4),
-                        color: color
-                      ),
-                      child: Center(child: Icon(Icons.volume_up,color: Colors.white, size: 30)),
-                      ),
+          margin: EdgeInsets.only(
+            right: MediaQuery.of(context).size.height * .01,
+          ),
+          padding: EdgeInsets.all(MediaQuery.of(context).size.height * .006),
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: bordercolor, width: 4),
+              color: color),
+          child: Center(
+              child: Icon(Icons.volume_up, color: Colors.white, size: 30)),
+        ),
       ),
     ],
   );
