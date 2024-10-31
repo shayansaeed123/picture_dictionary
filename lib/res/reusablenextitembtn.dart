1087cc132dcef8e69ab51f76a33d0bac4ad30c18
Shayan Saeed <bhattishayan274@gmail.com>
2024-10-31 17:10:24 +0500

    updated
    updated

--- lib/res/reusablenextitembtn.dart
+++ lib/res/reusablenextitembtn.dart
@@ -8,34 +8,4 @@ Widget reusablenextitembtn(BuildContext context, Function ontap, String image){
       ontap();
     },
     child: Image.asset(image, fit: BoxFit.contain,filterQuality: FilterQuality.high,));
-  // Container(
-  //                       padding: EdgeInsets.all(
-  //                           MediaQuery.of(context).size.height * .01),
-  //                       // height: MediaQuery.of(context).size.height * .07,
-  //                       decoration: BoxDecoration(
-  //                         shape: BoxShape.circle,
-  //                         border:
-  //                             Border.all(color: Color(0xFFffb64d), width: 4),
-  //                         gradient: LinearGradient(
-  //                           begin: Alignment.topCenter,
-  //                           end: Alignment.bottomCenter,
-  //                           colors: [
-  //                             Color(0xFFeb8815),
-  //                             Color(0xFFeb8815),
-  //                             // Color(0xFFe3e18e),
-  //                           ],
-  //                         ),
-  //                       ),
-  //                       child: Center(
-  //                           child: InkWell(
-  //                             onTap: (){
-  //                               return ontap();
-  //                             },
-  //                             child: Icon(
-  //                                                       icon,
-  //                                                       size: 35,
-  //                                                       color: Color(0xFFe3e18e),
-  //                                                     ),
-  //                           )),
-  //                     );
 }
